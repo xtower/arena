8bc9fe8fc42d89b97d403aca45b56f171fb9c1cf
Michal Matuszewski <Michal.Matuszewski@stepstone.com>
2018-09-27 17:30:26 +0200

    forgoten file

---
+++ src/main/java/com/example/arena/response/ApiError.java
@@ -0,0 +1,32 @@
+package com.example.arena.response;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Value;
+
+
+public class ApiError {
+  private HttpStatus status;
+  private String message;
+
+  public ApiError(HttpStatus status, String message) {
+    this.status = status;
+    this.message = message;
+  }
+
+  public HttpStatus getStatus() {
+    return status;
+  }
+
+  public void setStatus(HttpStatus status) {
+    this.status = status;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
